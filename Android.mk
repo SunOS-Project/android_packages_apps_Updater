9f9972cb5145306b8adf3790c9fde19c02f60130
cjh1249131356 <cjh1249131356@gmail.com>
2024-02-11 21:18:37 +0800

    Updater: Remove Android Studio build support
    Signed-off-by: cjh1249131356 <cjh1249131356@gmail.com>

--- Android.mk
+++ Android.mk
@@ -31,29 +31,3 @@ LOCAL_PRIVILEGED_MODULE := true
 LOCAL_CERTIFICATE := platform
 LOCAL_SYSTEM_EXT_MODULE := true
 LOCAL_PROGUARD_FLAG_FILES := proguard.flags
-
-include $(BUILD_PACKAGE)
-
-include $(CLEAR_VARS)
-
-LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES := \
-    gson:libs/gson.jar
-
-include $(BUILD_MULTI_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE := UpdaterStudio
-LOCAL_MODULE_CLASS := FAKE
-LOCAL_MODULE_SUFFIX := -timestamp
-updater_system_deps := $(call java-lib-deps,framework)
-updater_system_libs_path := $(abspath $(LOCAL_PATH))/system_libs
-
-include $(BUILD_SYSTEM)/base_rules.mk
-
-$(LOCAL_BUILT_MODULE): $(updater_system_deps)
-	$(hide) mkdir -p $(updater_system_libs_path)
-	$(hide) rm -rf $(updater_system_libs_path)/*.jar
-	$(hide) cp $(updater_system_deps) $(updater_system_libs_path)/framework.jar
-	$(hide) echo "Fake: $@"
-	$(hide) mkdir -p $(dir $@)
-	$(hide) touch $@
